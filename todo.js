81c48c99cc9df880d02610be74476165ad40e95e
Kunwar Yashaswee Chhaunker <75092847+misterfluffy25@users.noreply.github.com>
2021-01-16 04:10:41 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    alert("To-Do List : Loaded Successfully");
+}
+
+/*function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}*/
